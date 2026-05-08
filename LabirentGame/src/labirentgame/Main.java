b9cf175dfc4f9b1d802f958ba998a0a0dbb99c54
EmirHanDogan739 <quakeeses@gmail.com>
2026-05-08 18:31:54 +0300

    Main dosyası eklendi

---
+++ LabirentGame/src/labirentgame/Main.java
@@ -0,0 +1,15 @@
+
+package labirentgame;
+
+/**
+ *
+ * @author emird
+ */
+public class Main {
+    public static void main(String[] args){
+        java.awt.EventQueue.invokeLater(() -> {
+            new Pencere().setVisible(true);
+        
+        });
+    }
+}
